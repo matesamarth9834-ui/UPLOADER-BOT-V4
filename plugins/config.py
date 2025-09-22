8251e8749a6b949d3b66de74987991ba89532554
matesamarth9834-ui <matesamarth9834@gmail.com>
2025-09-22 16:17:29 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -12,14 +12,14 @@ logging.basicConfig(
 class Config(object):
     
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", "20246426" ))
+    API_HASH = os.environ.get("API_HASH", "77330c674fb1056fa7029a657075d2dc")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    MAX_FILE_SIZE = 2194304000
-    TG_MAX_FILE_SIZE = 2194304000
+    MAX_FILE_SIZE = 1259896345
+    TG_MAX_FILE_SIZE = 1259896345
     SESSION_STR = ""
-    FREE_USER_MAX_FILE_SIZE = 2194304000
+    FREE_USER_MAX_FILE_SIZE = 1259896345
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
@@ -39,7 +39,7 @@ class Config(object):
     SESSION_NAME = "UploaderXNTBot"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 
-    TG_MIN_FILE_SIZE = 2194304000
+    TG_MIN_FILE_SIZE = 1259896345
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     ADL_BOT_RQ = {}
 
